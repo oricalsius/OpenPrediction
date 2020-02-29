4f59686bcbb6c32d7974954656f40301451850e6
calsius <38387914+oricalsius@users.noreply.github.com>
2020-02-29 21:04:04 +0100

    main example

--- main.py
+++ main.py
@@ -28,25 +28,25 @@ def load_original_data_cryptocompare():
 
 
 if __name__ == "__main__":
-    window = 4; verbose = True; early_stop = True; load_model = False; save_models = True
+    window = 4; verbose = True; early_stop = True; tick_to_display = 600;
 
     # Define the ML model to use
-    ml_model = MultiLayerPerceptronNN(hidden_layer_sizes=(280, 75), activation='logistic',
+    ml_model = MultiLayerPerceptronNN(hidden_layer_sizes=(280, 70), activation='logistic',
                                       solver="adam", random_state=0, verbose=verbose,
-                                      learning_rate_init=1e-3, early_stopping=early_stop, validation_fraction=0.2,
+                                      learning_rate_init=1e-3, early_stopping=early_stop, validation_fraction=0.20,
                                       tol=1e-6, alpha=0.0001, learning_rate="adaptive", max_iter=10000,
                                       batch_size=75, n_iter_no_change=10)
 
     # Get the exchange data
-    original_data = load_original_data_huobi()
+    exchange_data = load_original_data_huobi()
 
     # Initialize the Architect object
-    arc = MLArchitect("saved_data/x.csv", "saved_data/y.csv", is_x_flat=False,
+    arc = MLArchitect(x_data=exchange_data, y_data=None, is_x_flat=True, save_x_path="saved_data/x.csv",
                       save_indicators_path="saved_data/indicators.csv", save_y_path="saved_data/y.csv",
-                      save_normalize_x_model="saved_data/x_norm_model.mod", y_restoration_routine=None,
+                      save_normalize_x_model="saved_data/x_norm_model.mod", y_restoration_routine="default",
                       save_normalize_y_model="saved_data/y_norm_model.mod",
-                      index_col='id', index_infer_datetime_format=True,
-                      window_prediction=4, test_size=0.01, ml_model=ml_model)
+                      index_col='id', index_infer_datetime_format=True, pca_n_components=0.99,
+                      window_prediction=4, test_size=0.01, ml_model=ml_model, save_ml_path="saved_data/ml_model.mod")
 
     # Fit the model
     arc.ml_fit()
@@ -54,25 +54,19 @@ if __name__ == "__main__":
     score_train = arc.ml_score_train(); score_test = arc.ml_score_test()
     print(f"score train: {score_train}\nscore test: {score_test}")
 
-
     # Predict future prices
-    y_predicted = arc.ml_predict()
+    y_predicted = arc.ml_predict(restore_y=True, original_data=exchange_data, windows=window, is_returns=True)
 
+    # Get all data
+    data_with_indicators = pd.concat([arc.data_with_indicators, y_predicted], axis=1, sort=False)
 
-    src_columns = ["open", "close", "high", "low"]
-    columns_equivalences = []
-    columns_equivalences = [(src_columns, [x + "_log_returns_" + str(i + 1) for x in src_columns], i+1)
-                            for i in range(window)]
+    # Display graphs
+    scatters_plot = dict(zip(y_predicted.columns, y_predicted.columns))
+    #sub_plots = [{"Count": "count", "QCount1": "QCount1", "QCount95": "QCount95"}, {"Vol": "vol", "QVol95": "QVol95", "QVol1": "QVol1"}]
+    sub_plots = []
 
-    #data_with_indicators = pd.concat([data_with_indicators, y_restored], axis=1, sort=False)
-
-    # scatters_plot = dict(zip(y_restored.columns, y_restored.columns))
-    # #sub_plots = [{"Count": "count", "QCount1": "QCount1", "QCount95": "QCount95"}, {"Vol": "vol", "QVol95": "QVol95", "QVol1": "QVol1"}]
-    # sub_plots = []
-    #
-    # data_to_display = data_with_indicators.iloc[-600:, :]
-    # display_graph(data_to_display, data_index=data_to_display.index, open='open',
-    #               close='close', high='high', low='low', scatters_plot=scatters_plot, sub_plots=sub_plots)
+    data_to_display = data_with_indicators.iloc[-tick_to_display:, :]
+    display_graph(data_to_display, data_index=data_to_display.index, scatters_plot=scatters_plot, sub_plots=sub_plots)
 
     print("")
 
