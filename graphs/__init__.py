323338fc22fdd9e8926041d9149e9d84ce394b38
calsius <38387914+oricalsius@users.noreply.github.com>
2020-03-04 23:18:57 +0100

    color candlestick with two different colors

--- graphs/__init__.py
+++ graphs/__init__.py
@@ -5,7 +5,8 @@ from pandas import DataFrame, Series
 
 
 def display_graph(data_to_display: DataFrame, data_index: Series, open: str = 'open', close: str = 'close',
-                  high: str = 'high', low: str = 'low', scatters_plot: dict = {}, sub_plots: List[dict] = []):
+                  high: str = 'high', low: str = 'low', scatters_plot: dict = {}, sub_plots: List[dict] = [],
+                  data_index_cyan: Series = None, data_index_green: Series = None):
 
     empty_parameters = [key for key, value in
                         {"open": open, "close": close, "high": high, "low": low}.items()
@@ -20,8 +21,16 @@ def display_graph(data_to_display: DataFrame, data_index: Series, open: str = 'o
     fig = make_subplots(rows=len(sub_plots) + 4, cols=1, shared_xaxes=True, vertical_spacing=0.01, specs=specs)
 
     # Add Candlestick graph
-    fig.add_trace(go.Candlestick(x=data_index, open=data_to_display[open], high=data_to_display[high],
-                                 low=data_to_display[low], close=data_to_display[close]), row=1, col=1)
+    data_index_green = data_index if data_index_cyan is None else data_index_green
+
+    data_candle = data_to_display.loc[data_index_green, :]
+    fig.add_trace(go.Candlestick(x=data_index_green, open=data_candle[open], high=data_candle[high],
+                                 low=data_candle[low], close=data_candle[close]), row=1, col=1)
+
+    data_cyan = data_to_display.loc[data_index_cyan, :]
+    fig.add_trace(go.Candlestick(x=data_index_cyan, open=data_cyan[open], high=data_cyan[high], low=data_cyan[low],
+                                 close=data_cyan[close],
+                                 increasing_line_color='cyan', decreasing_line_color='gray'), row=1, col=1)
 
     # Add other scatters plots in the main graph
     for label, column_name in scatters_plot.items():
