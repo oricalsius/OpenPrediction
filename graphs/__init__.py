5c4f562f8684b98648cd82184be92a84192a0088
calsius <38387914+oricalsius@users.noreply.github.com>
2020-03-05 16:39:10 +0100

    refactoring

--- graphs/__init__.py
+++ graphs/__init__.py
@@ -1,12 +1,12 @@
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from typing import List
-from pandas import DataFrame, Series
+from pandas import DataFrame, Series, Index
 
 
-def display_graph(data_to_display: DataFrame, data_index: Series, open: str = 'open', close: str = 'close',
+def display_graph(data_to_display: DataFrame, open: str = 'open', close: str = 'close',
                   high: str = 'high', low: str = 'low', scatters_plot: dict = {}, sub_plots: List[dict] = [],
-                  data_index_cyan: Series = None, data_index_green: Series = None):
+                  data_index_cyan: Series = None):
 
     empty_parameters = [key for key, value in
                         {"open": open, "close": close, "high": high, "low": low}.items()
@@ -15,22 +15,29 @@ def display_graph(data_to_display: DataFrame, data_index: Series, open: str = 'o
     if empty_parameters:
         raise Exception("Following columns names should not be empty: {0}".format(empty_parameters))
 
+    # Index definition
+    data_index = data_to_display.index
+
+    if data_index_cyan is None:
+        data_index_green = data_index
+    else:
+        data_index_green = set(data_to_display.index) - set(data_index_cyan)
+        data_index_green = Index(data_index_green).sort_values()
+
     # Create the template for the subplots. The main figure will take 3 rows.
     specs = [[{"rowspan": 3}], [None], [None], [{}]]
     specs.extend([{}] for i in range(len(sub_plots)))
     fig = make_subplots(rows=len(sub_plots) + 4, cols=1, shared_xaxes=True, vertical_spacing=0.01, specs=specs)
 
     # Add Candlestick graph
-    data_index_green = data_index if data_index_cyan is None else data_index_green
-
     data_candle = data_to_display.loc[data_index_green, :]
     fig.add_trace(go.Candlestick(x=data_index_green, open=data_candle[open], high=data_candle[high],
                                  low=data_candle[low], close=data_candle[close]), row=1, col=1)
 
     data_cyan = data_to_display.loc[data_index_cyan, :]
     fig.add_trace(go.Candlestick(x=data_index_cyan, open=data_cyan[open], high=data_cyan[high], low=data_cyan[low],
-                                 close=data_cyan[close],
-                                 increasing_line_color='cyan', decreasing_line_color='gray'), row=1, col=1)
+                                 close=data_cyan[close], increasing_line_color='cyan',
+                                 decreasing_line_color='gray'), row=1, col=1)
 
     # Add other scatters plots in the main graph
     for label, column_name in scatters_plot.items():
