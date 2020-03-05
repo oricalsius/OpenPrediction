182ca43f132e9cfd39aa1b7471ae3f9e866427a1
calsius <38387914+oricalsius@users.noreply.github.com>
2020-03-05 16:55:50 +0100

    new main

--- main_exec.py
+++ main_exec.py
@@ -34,13 +34,6 @@ def run_main():
     early_stop = True;
     tick_to_display = 600;
 
-    # Define the ML model to use
-    ml_model = MultiLayerPerceptronNN(hidden_layer_sizes=(280, 70), activation='logistic',
-                                      solver="adam", random_state=0, verbose=verbose,
-                                      learning_rate_init=1e-3, early_stopping=early_stop, validation_fraction=0.20,
-                                      tol=1e-6, alpha=0.0001, learning_rate="adaptive", max_iter=10000,
-                                      batch_size=75, n_iter_no_change=10)
-
     # Get the exchange data
     exchange_data = load_original_data_huobi()
 
@@ -50,7 +43,18 @@ def run_main():
                       save_normalize_x_model="saved_data/x_norm_model.mod", y_restoration_routine="default",
                       save_normalize_y_model="saved_data/y_norm_model.mod",
                       index_col='id', index_infer_datetime_format=True, add_pca=True, pca_n_components=None,
-                      window_prediction=4, test_size=0.10, ml_model=ml_model, save_ml_path="saved_data/ml_model.mod")
+                      window_prediction=4, test_size=0.10, ml_model=None, save_ml_path="saved_data/ml_model.mod")
+
+    # Define the ML model to use
+    nb_layers = arc.norm_input_transform(arc.x_test).shape[1]
+    ml_model = MultiLayerPerceptronNN(hidden_layer_sizes=(nb_layers*3, nb_layers), activation='logistic',
+                                      solver="adam", random_state=0, verbose=verbose,
+                                      learning_rate_init=1e-3, early_stopping=early_stop, validation_fraction=0.20,
+                                      tol=1e-6, alpha=0.0001, learning_rate="adaptive", max_iter=10000,
+                                      batch_size=75, n_iter_no_change=10)
+
+    # Init the ML model
+    arc.ml_init_model(ml_model)
 
     # Fit the model
     arc.ml_fit()
@@ -63,28 +67,24 @@ def run_main():
 
     # Get all data
     data_with_indicators = pd.concat([arc.data_with_indicators, y_predicted], axis=1, sort=False)
-    #data_with_indicators = arc.data_with_indicators
 
     # Display graphs
     scatters_plot = dict(zip(y_predicted.columns, y_predicted.columns))
-    #scatters_plot = dict(zip(data_with_indicators.columns, data_with_indicators.columns))
 
     src_columns = ["open", "close", "high", "low"]
     rdix = [x + "_rdix_5" for x in src_columns] + [x + "_rdix_avg_14" for x in src_columns] \
            + [x + "_square_rdix_5" for x in src_columns] + [x + "_square_rdix_avg_14" for x in src_columns] \
            + [x + "_norm_rdix_5" for x in src_columns] + [x + "_norm_rdix_avg_14" for x in src_columns] \
            + [x + "_PriceVelo_5" for x in src_columns] + [x + "_PriceVelo_avg_14" for x in src_columns] \
-           + [x + "_ReturnsVelo_5" for x in src_columns] + [x + "_ReturnsVelo_avg_14" for x in src_columns]
+           + [x + "_ReturnsVelo_5" for x in src_columns] + [x + "_ReturnsVelo_avg_14" for x in src_columns] \
+           + ["RSI_14"]
 
-    sub_plots = []
     sub_plots = [dict(zip(rdix, rdix))]
 
     data_to_display = data_with_indicators.iloc[-tick_to_display:, :]
-    data_index_green = (arc.x_train.index & data_to_display.index).sort_values()
     data_index_cyan = (arc.x_test.index & data_to_display.index).sort_values()
 
-    display_graph(data_to_display, data_index=data_to_display.index, scatters_plot=scatters_plot, sub_plots=sub_plots,
-                  data_index_green=data_index_green, data_index_cyan=data_index_cyan)
+    display_graph(data_to_display, scatters_plot=scatters_plot, sub_plots=sub_plots, data_index_cyan=data_index_cyan)
 
 
 if __name__ == "__main__":
